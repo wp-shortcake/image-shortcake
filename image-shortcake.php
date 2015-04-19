10b2e7f234411dd56722d68304c4c65f82053323
Daniel Bachhuber <d@danielbachhuber.com>
2015-04-19 13:40:11 -0700

    Scaffold plugin via WP-CLI

---
+++ image-shortcake.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Image-shortcake
+Version: 0.1-alpha
+Description: PLUGIN DESCRIPTION HERE
+Author: YOUR NAME HERE
+Author URI: YOUR SITE HERE
+Plugin URI: PLUGIN SITE HERE
+Text Domain: image-shortcake
+Domain Path: /languages
+*/
+
