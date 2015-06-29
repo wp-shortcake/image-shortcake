846b94249ccc08f204535772c650ff07834e72fd
Nathaniel Taintor <goldenapplesdesign@gmail.com>
2015-06-28 20:16:31 -0600

    Set alt and caption fields on selecting attachment
    When selecting a new attachment for an [img] shortcode, the alt and
    caption attributes should be inherited from the attachment post.

--- assets/js/image-shortcake-admin.js
+++ assets/js/image-shortcake-admin.js
@@ -3,6 +3,32 @@ var ImageShortcake = {
 
 	listeners: {
 		/**
+		 * Callback for the "attachment" attribute field.
+		 *
+		 * When selecting an attachment for an [img], try to populate image
+		 * shortcode attribute fields (alt, caption, description,etc.) from
+		 * the attachment data.
+		 */
+		attachment: function( changed, collection, shortcode ) {
+			if ( typeof changed.value === 'undefined' || typeof window.sui.data.idCache[ changed.value ] === 'undefined' ) {
+				return;
+			}
+			var attachment = window.sui.data.idCache[ changed.value ];
+
+			var altField = _.find( collection, function( viewModel ) { return 'alt' === viewModel.model.get('attr'); } );
+			var captionField = _.find( collection, function( viewModel ) { return 'caption' === viewModel.model.get('attr'); } );
+
+			if ( ! altField.model.get('value') && attachment.alt ) {
+				altField.$el.find('[name="alt"]').val( attachment.alt );
+			}
+
+			if ( ! captionField.model.get('value') && attachment.caption ) {
+				captionField.$el.find('[name="caption"]').val( attachment.caption );
+			}
+
+		},
+
+		/**
 		 * Callback for the "linkto" attribute field.
 		 *
 		 * Display the "Custom Link" field if and only if the "linkto" field is "custom"
@@ -27,6 +53,7 @@ var ImageShortcake = {
  */
 if ( typeof wp.shortcake !== 'undefined' && typeof wp.shortcake.hooks !== 'undefined' ) {
 
-	wp.shortcake.hooks.addAction( 'img.linkto', ImageShortcake.listeners.linkto );
+	wp.shortcake.hooks.addAction( 'img.attachment', ImageShortcake.listeners.attachment );
+	wp.shortcake.hooks.addAction( 'img.linkto',     ImageShortcake.listeners.linkto     );
 
 }
